c643da32dd2df435650cdd101561c9d6ba98104d
Helene Athalia <g00440625@atu.ie>
2026-01-05 23:53:53 +0000

    wleek plan container for 7-day with breakfast, lunch, dinner slots

---
+++ MealPlanner/Models/DayMeal.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MealPlanner.Models
+{
+	public class DayMeal : INotifyPropertyChanged
+	{
+		public string Day { get; set; } = string.Empty;
+
+		private Recipe? _breakfast;
+		public Recipe? Breakfast
+		{
+			get => _breakfast;
+			set { if (_breakfast != value) { _breakfast = value; OnPropertyChanged(); } }
+		}
+
+		private Recipe? _lunch;
+		public Recipe? Lunch
+		{
+			get => _lunch;
+			set { if (_lunch != value) { _lunch = value; OnPropertyChanged(); } }
+		}
+
+		private Recipe? _dinner;
+		public Recipe? Dinner
+		{
+			get => _dinner;
+			set { if (_dinner != value) { _dinner = value; OnPropertyChanged(); } }
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string? name = null)
+			=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	}
+
+	public class WeekPlan
+	{
+		public List<DayMeal> Days { get; set; } = new();
+	}
+}
+
